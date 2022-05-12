34496cd8aac7462686b0b477685b770d7f1b67d9
rafaelspa <rsdepaula67@gmail.com>
2022-05-12 14:26:38 -0300

    Apresentando o Thymeleaf na renderização do html

--- src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
+++ src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
@@ -2,13 +2,25 @@ package br.com.xavecoding.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        System.out.println("**************************");
-        return "hello"; // o Spring vai renderizar o arquivo templates/hello.html
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello"); // nome do arquivo html a ser renderizado/exibido
+        mv.addObject("nome", "Maria");
+        return mv; // o Spring vai renderizar o arquivo templates/hello.html
     }
+/*    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Samuka");
+        return "hello"; // o Spring vai renderizar o arquivo templates/hello.html
+    }*/
+/*    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Zezinho");
+        return "hello"; // o Spring vai renderizar o arquivo templates/hello.html
+    }*/
 }
