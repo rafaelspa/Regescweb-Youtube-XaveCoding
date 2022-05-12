9b133c506ec633240f1c57fd773c54bca124a993
rafaelspa <rsdepaula67@gmail.com>
2022-05-12 15:35:17 -0300

    Criando uma entidade com Spring Data

---
+++ src/main/java/br/com/xavecoding/regescweb/models/Professor.java
@@ -0,0 +1,52 @@
+package br.com.xavecoding.regescweb.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    private BigDecimal salario;
+    @Enumerated(EnumType.STRING)
+    private StatusOperator statusOperator;
+
+    public Professor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public StatusOperator getStatusOperator() {
+        return statusOperator;
+    }
+
+    public void setStatusOperator(StatusOperator statusOperator) {
+        this.statusOperator = statusOperator;
+    }
+}
---
+++ src/main/java/br/com/xavecoding/regescweb/models/StatusOperator.java
@@ -0,0 +1,5 @@
+package br.com.xavecoding.regescweb.models;
+
+public enum StatusOperator {
+    ATIVO, INATIVO, APOSENTADO, AFASTADO;
+}
